8c1203a386e1e372e7680c61468e5607cc19acbb
Caleumtatsu2010 <shinyakogami2010@gmail.com>
2023-06-21 10:49:53 +0700

    .

--- src/main/java/com/caleumtatsu2010/cassandra/models/cart/item/CASCartItemQueries.java
+++ src/main/java/com/caleumtatsu2010/cassandra/models/cart/item/CASCartItemQueries.java
@@ -6,9 +6,9 @@ public class CASCartItemQueries {
 	
 	public static String selectById = "select * from techmate.cart_item where id = ?;";
 	
-	public static String insertById = "INSERT INTO techmate.cart_item (id, cart_id, product_id) VALUES (?, ?, ?)";
+	public static String insertById = "INSERT INTO techmate.cart_item (id, cart_id, product_id, num) VALUES (?, ?, ?, ?)";
 	
-	public static String updateById = "UPDATE techmate.cart_item SET cart_id = ?, product_id = ? WHERE id = ? IF EXISTS;";
+	public static String updateById = "UPDATE techmate.cart_item SET cart_id = ?, product_id = ?, num = ? WHERE id = ? IF EXISTS;";
 	
 	public static String deleteByID = "DELETE FROM techmate.cart_item where id = ?;";
 }
